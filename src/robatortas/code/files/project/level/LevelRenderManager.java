59a318e3e5ad06ab409f9e4a419996aed8aacd84
Robatortas <robatortas1@gmail.com>
2022-03-13 03:03:06 -0700

    Level Rendering cleanup

--- src/robatortas/code/files/project/level/LevelRenderManager.java
+++ src/robatortas/code/files/project/level/LevelRenderManager.java
@@ -5,8 +5,24 @@ import robatortas.code.files.core.render.RenderManager;
 
 public class LevelRenderManager {
 	
+	private RenderManager screen;
+	
+	public LevelRenderManager(RenderManager screen) {
+		this.screen = screen;
+	}
+	
 	// Level Rendering goes here!
-	public void render(int x, int y, RenderManager screen, LevelManager level) {
+	public void render(int x, int y, LevelManager level) {
 		level.getLevel(x, y).render(x, y, screen);
+	}
+	
+	// pinPoint settings
+	public int x0,y0,x1,y1; 
+	public void pinPoints(int xScroll, int yScroll) {
+		screen.setOffset(xScroll, yScroll);
+		x0 = xScroll >> 4;
+		x1 = (xScroll + screen.width + 16) >> 4;
+		y0 = yScroll >> 4;
+		y1 = (yScroll + screen.height + 16) >> 4;
 	}
 }
