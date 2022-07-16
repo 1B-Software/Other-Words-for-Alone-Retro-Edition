134f4a6c2d6d41cc943d18a7cd663e05d71d45e1
Robatortas <robatortas1@gmail.com>
2022-07-16 11:17:35 -0800

    Stamina bar render done

--- src/robatortas/code/files/core/console/Console.java
+++ src/robatortas/code/files/core/console/Console.java
@@ -1,5 +1,6 @@
 package robatortas.code.files.core.console;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 // CONSOLE IS IN TESTDEV!
@@ -29,28 +30,35 @@ public class Console implements Runnable {
 		System.out.println("[" + from + "]" + ": " + msg);
 	}
 	
-	private static String[] cmd = new String[] {
-			"help", "exit", "hello world"
-			};
-	
-	// COMMANDS ALPHA
-	public void commands() {
-		if(!s.startsWith("!")) writePlayerMsg(s);
-		
-		if(readNextLine(0)) writeSysMsg(" I am glad to help here, tho I can't atm!");
-		if(readNextLine(1)) {
-			writeSysMsg(" Quitting...");
-			System.exit(0);
-		}
-		if(readNextLine(2)) writeSysMsg(" Hello fellow human!");
-			
-	}
-	
 	private boolean readNextLine(int index) {
 		if(s.startsWith("!")) return s.contains(cmd[index].toLowerCase());
 		return false;
 	}
 
+	private static String[] cmd = new String[] {
+			"help", "exit", "hello"
+			};
+	
+	private String getCommandList() {
+		return Arrays.toString(cmd);
+	}
+	
+	// COMMANDS ALPHA
+	public void commands() {
+		if(!s.startsWith("!")) writePlayerMsg(s);
+		
+		if(readNextLine(0)) {
+			writeSysMsg("Here is the list of commands: \n"
+					+ getCommandList());
+		}
+		if(readNextLine(1)) {
+			writeSysMsg("Quitting...");
+			System.exit(0);
+		}
+		if(readNextLine(2)) writeSysMsg("Hello fellow human!");
+			
+	}
+	
 	public synchronized void start() {
 		thread = new Thread(this, "Console");
 		thread.start();
