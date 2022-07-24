b1f6e1784258069be0b550f0440b7e71f468a869
Robatortas <robatortas1@gmail.com>
2022-07-24 13:32:27 -0800

    Removed Proto Desert folder and its contents

--- src/robatortas/code/files/project/DisplayManager.java
+++ src/robatortas/code/files/project/DisplayManager.java
@@ -2,15 +2,16 @@ package robatortas.code.files.project;
 
 import java.awt.Dimension;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
-import robatortas.code.files.core.console.Console;
-import robatortas.code.files.project.level.TerrainGen.NoiseMap;
 import robatortas.code.files.project.settings.Constants;
 
 public class DisplayManager {
 	
 	private GameManager game;
+	
+	private ImageIcon windowIcon = new ImageIcon(getClass().getClassLoader().getResource("textures/icon/window_icon.png"));
 	
 	public DisplayManager(int width, int height, String title, GameManager game) {
 		this.game = game;
@@ -19,6 +20,7 @@ public class DisplayManager {
 		
 		Dimension size = new Dimension(width*Constants.SCALE, height*Constants.SCALE);
 		
+		game.frame.setIconImage(windowIcon.getImage());
 		game.frame.setTitle(title);
 		game.frame.pack();
 		game.frame.add(game);
