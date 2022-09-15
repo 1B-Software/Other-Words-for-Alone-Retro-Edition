f06603b720e77b6263dcebfcd0fef00a2eb3deb6
Robatortas <robatortas1@gmail.com>
2022-09-14 22:51:53 -0700

    Laptop

--- src/robatortas/code/files/project/DisplayManager.java
+++ src/robatortas/code/files/project/DisplayManager.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
+import robatortas.code.files.project.level.TerrainGen.NoiseMap;
 import robatortas.code.files.project.settings.Constants;
 
 public class DisplayManager {
